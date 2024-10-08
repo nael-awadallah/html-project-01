acdc300cde7095a9caecedbc4c120bedbc857634
Nael Awadallah <cto@blackink.agency>
2024-10-09 01:03:07 +0300

    eee

--- dist/script.c10090b4.js
+++ dist/script.c10090b4.js
@@ -144,7 +144,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54244" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55404" + '/');
   ws.onmessage = function (event) {
     checkedAssets = {};
     assetsToAccept = [];
